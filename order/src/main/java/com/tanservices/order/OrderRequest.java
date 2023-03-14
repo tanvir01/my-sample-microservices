bb5cae2466e0e9127f2eb55ba9809c48b71ca7e5
tanvir <t.rabbi@sevensenders.com>
2023-03-14 22:49:49 +0100

    updated order ms and added shipment ms

--- order/src/main/java/com/tanservices/order/OrderRequest.java
+++ order/src/main/java/com/tanservices/order/OrderRequest.java
@@ -3,6 +3,5 @@ package com.tanservices.order;
 public record OrderRequest(
         String customerName,
         String customerEmail,
-        String shippingAddress,
         Double totalAmount) {
 }
