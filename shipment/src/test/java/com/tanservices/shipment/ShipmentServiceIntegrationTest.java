8aa0914a66da6716f0736092fd803228944669f8
tanvir <t.rabbi@sevensenders.com>
2023-03-21 22:55:40 +0100

    remove unnecessary imports

--- shipment/src/test/java/com/tanservices/shipment/ShipmentServiceIntegrationTest.java
+++ shipment/src/test/java/com/tanservices/shipment/ShipmentServiceIntegrationTest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
