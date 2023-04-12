37375915573281d6bbab3dad6333daf450ae9ca7
tanvir <t.rabbi@sevensenders.com>
2023-04-13 00:37:36 +0200

    refactoring order ms

---
+++ order/src/main/java/com/tanservices/order/security/FetchCustomerInfo.java
@@ -0,0 +1,26 @@
+package com.tanservices.order.security;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FetchCustomerInfo {
+
+    private Claims claims;
+
+    public void setClaims(Claims claims) {
+        this.claims = claims;
+    }
+
+    public String getCustomerName() {
+        return (String) this.claims.get("name");
+    }
+
+    public String getCustomerEmail() {
+        return (String) this.claims.get("email");
+    }
+
+    public String getCustomerAddress() {
+        return (String) this.claims.get("address");
+    }
+}
