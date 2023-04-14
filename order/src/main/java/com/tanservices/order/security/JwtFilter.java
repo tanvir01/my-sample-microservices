5731241950e7e3643d0b3be6359b4a43566abf7f
tanvir <t.rabbi@sevensenders.com>
2023-04-14 02:20:55 +0200

    introducing JwtContextHolder

---
+++ order/src/main/java/com/tanservices/order/security/JwtFilter.java
@@ -0,0 +1,38 @@
+package com.tanservices.order.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            if (jwtService.validateToken(token)) {
+                filterChain.doFilter(request, response);
+                JwtContextHolder.clear();
+                return;
+            }
+        }
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    }
+}
+
